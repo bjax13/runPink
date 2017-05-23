644db54ba770b0697a35b2c677e9a4a1d81349ad
Bryan Jackson <bjack13@gmail.com>
2017-05-23 10:19:11 -0600

    setting background images for results page

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -11,22 +11,16 @@ export class ResultsComponent implements OnInit {
     [2015,
       [
         [
+          'SPRING 5K/10K PLEASANT GROVE',
+          'October 10, 2015' ,
+          'www.google.com',
+          'assets/images/results/group-2.jpg'
+        ],
+        [
           'PARK CITY HALF MARATHON, 5K/10K',
           'October 10, 2015' ,
           'www.google.com',
-          ''
-        ],
-        [
-          'SPRING 5K/10K PLEASANT GROVE',
-          'October 10, 2015' ,
-          'www.google.com',
-          ''
-        ],
-        [
-          'SPRING 5K/10K PLEASANT GROVE',
-          'October 10, 2015' ,
-          'www.google.com',
-          ''
+          'assets/images/results/group-3.jpg'
         ],
       ]
     ],
@@ -36,85 +30,50 @@ export class ResultsComponent implements OnInit {
           'UTAH COUNTY 5K/10K',
           'April 19, 2014' ,
           'www.google.com',
-          ''
+          'assets/images/results/group-4.jpg'
+        ],
+        [
+          'PARK CITY HALF MARATHON AND 5K',
+          'October 11, 2014' ,
+          'www.google.com',
+          'assets/images/results/group-5.jpg'
         ],
 
       ]
     ],
-    ['Past Charites',
+    [2013,
       [
         [
-          'UTAH COUNTY 5K/10K',
-          'April 19, 2014' ,
+          'PLEASANT GROVE 5K',
+          'April 27, 2013' ,
           'www.google.com',
-          ''
+          'assets/images/results/group-6.jpg'
+        ],
+        [
+          'PARK CITY HALF MARATHON AND 5K',
+          'October 12, 2013' ,
+          'www.google.com',
+          'assets/images/results/group-7.jpg'
         ],
 
       ]
     ],
-    [2014,
+    [2012,
       [
         [
-          'UTAH COUNTY 5K/10K',
-          'April 19, 2014' ,
+          'PLEASANT GROVE 5K',
+          'April 27, 2012' ,
           'www.google.com',
-          ''
+          'assets/images/results/group-6.jpg'
         ],
         [
           'PARK CITY HALF MARATHON AND 5K',
-          'October 11, 2014' ,
+          'October 13, 2012' ,
           'www.google.com',
-          ''
-        ],
-        [
-          'PARK CITY HALF MARATHON AND 5K',
-          'October 11, 2014' ,
-          'www.google.com',
-          ''
-        ],
-        [
-          'PARK CITY HALF MARATHON AND 5K',
-          'October 11, 2014' ,
-          'www.google.com',
-          ''
+          'assets/images/results/group-7.jpg'
         ],
       ]
     ],
-    [2014,
-      [
-        [
-          'UTAH COUNTY 5K/10K',
-          'April 19, 2014' ,
-          'www.google.com',
-          ''
-        ],
-        [
-          'PARK CITY HALF MARATHON AND 5K',
-          'October 11, 2014' ,
-          'www.google.com',
-          ''
-        ],
-        [
-          'PARK CITY HALF MARATHON AND 5K',
-          'October 11, 2014' ,
-          'www.google.com',
-          ''
-        ],
-        [
-          'PARK CITY HALF MARATHON AND 5K',
-          'October 11, 2014' ,
-          'www.google.com',
-          ''
-        ],
-        [
-          'PARK CITY HALF MARATHON AND 5K',
-          'October 11, 2014' ,
-          'www.google.com',
-          ''
-        ],
-      ]
-    ],
-
   ]
 
   constructor() { }
