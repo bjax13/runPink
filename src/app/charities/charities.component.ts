1e4fe3193c77c6012d9fafe522fb88e86c3150bd
Bryan Jackson <bjack13@gmail.com>
2017-05-23 11:09:42 -0600

    updating banner images

--- src/app/charities/charities.component.ts
+++ src/app/charities/charities.component.ts
@@ -13,12 +13,16 @@ export class CharitiesComponent implements OnInit {
         [
           'Pink Series Charity of 2014',
           'THE CHRISTI ANDERSON RACK PACK FOUNDATION' ,
-          'www.google.com'
+          'www.google.com',
+          'assets/images/banners/solidPink.png',
+          'VISIT PAGE'
         ],
         [
           'Pink Series Charity of 2013',
           'IMAGE REBORN FOUNDATION' ,
-          'www.google.com'
+          'www.google.com',
+          'assets/images/banners/solidPink.png',
+          'VISIT PAGE'
         ],
 
       ]
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -14,13 +14,15 @@ export class ResultsComponent implements OnInit {
           'SPRING 5K/10K PLEASANT GROVE',
           'October 10, 2015' ,
           'www.google.com',
-          'assets/images/results/group-2.jpg'
+          'assets/images/results/group-2.jpg',
+          'SEE RESULTS'
         ],
         [
           'PARK CITY HALF MARATHON, 5K/10K',
           'October 10, 2015' ,
           'www.google.com',
-          'assets/images/results/group-3.jpg'
+          'assets/images/results/group-3.jpg',
+          'SEE RESULTS'
         ],
       ]
     ],
@@ -30,13 +32,15 @@ export class ResultsComponent implements OnInit {
           'UTAH COUNTY 5K/10K',
           'April 19, 2014' ,
           'www.google.com',
-          'assets/images/results/group-4.jpg'
+          'assets/images/results/group-4.jpg',
+          'SEE RESULTS'
         ],
         [
           'PARK CITY HALF MARATHON AND 5K',
           'October 11, 2014' ,
           'www.google.com',
-          'assets/images/results/group-5.jpg'
+          'assets/images/results/group-5.jpg',
+          'SEE RESULTS'
         ],
 
       ]
@@ -47,13 +51,15 @@ export class ResultsComponent implements OnInit {
           'PLEASANT GROVE 5K',
           'April 27, 2013' ,
           'www.google.com',
-          'assets/images/results/group-6.jpg'
+          'assets/images/results/group-6.jpg',
+          'SEE RESULTS'
         ],
         [
           'PARK CITY HALF MARATHON AND 5K',
           'October 12, 2013' ,
           'www.google.com',
-          'assets/images/results/group-7.jpg'
+          'assets/images/results/group-7.jpg',
+          'SEE RESULTS'
         ],
 
       ]
@@ -64,13 +70,15 @@ export class ResultsComponent implements OnInit {
           'PLEASANT GROVE 5K',
           'April 27, 2012' ,
           'www.google.com',
-          'assets/images/results/group-6.jpg'
+          'assets/images/results/group-6.jpg',
+          'SEE RESULTS'
         ],
         [
           'PARK CITY HALF MARATHON AND 5K',
           'October 13, 2012' ,
           'www.google.com',
-          'assets/images/results/group-7.jpg'
+          'assets/images/results/group-7.jpg',
+          'SEE RESULTS'
         ],
       ]
     ],
